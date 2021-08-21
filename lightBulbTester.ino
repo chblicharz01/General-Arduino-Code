86163e8080c2e8d2c787315c509fa00560ca4e59
Chris Blicharz <41934304+chblicharz01@users.noreply.github.com>
2021-08-21 12:25:50 -0500

    Add files via upload

---
+++ lightBulbTester.ino
@@ -0,0 +1,16 @@
+const int kLED1 = 11;
+
+
+void setup() 
+{
+  pinMode(kLED1, OUTPUT);
+
+}
+
+void loop() 
+{
+  digitalWrite (kLED1, HIGH);
+  delay(1000);
+  digitalWrite (kLED1, LOW);
+  delay(1000);
+}
